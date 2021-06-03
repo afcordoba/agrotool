32ff52cb092cef764316b17e267afb480e78b0d1
acordoba <acordoba@c00654.of1.net>
2021-06-03 19:09:29 -0300

    Setup inicial

---
+++ agrotool/src/main/java/com/agrotool/AgrotoolApplication.java
@@ -0,0 +1,13 @@
+package com.agrotool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrotoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrotoolApplication.class, args);
+	}
+
+}
---
+++ agrotool/src/test/java/com/agrotool/AgrotoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrotool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrotoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
